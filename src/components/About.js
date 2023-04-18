53bb8cd7675ef867e960a28434a6e5d042a80303
Emmanuel <emmanuel.mukira@student.moringaschool.com>
2023-04-18 15:00:48 +0300

    Links

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,17 @@
 import React from "react";
+import Links from "/components/Links";
 
-function About() {
+function About(props) {
+  const { bio } = props;
+const isBio = bio && bio.trim() !== '';
   return (
     <div id="about">
       <h2>About Me</h2>
+      {isBio && <p>{bio}</p>}
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
+      {<Links/>}
+   </div>
   );
 }
-
 export default About;
